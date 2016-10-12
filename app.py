4ee0758c4379c1e79ff3503fbbcdc05a9eb8d9f5
askprateek <askpk123@gmail.com>
2016-10-12 22:17:37 +0530

    Automatic Control panel generation

--- app.py
+++ app.py
@@ -1,44 +1,43 @@
 from flask import Flask, render_template, request
-import os, requests
+import os, requests, json, ssl
+from flask.json import jsonify
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
 app = Flask(__name__, static_url_path='')
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == 'admin' and password == 'password'
+# url_for('static', filename='style.css')
 
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
-
-
-
-piserver = "https://172.16.15.252:8000/"                #BACKEND | PISERVER URL HERE
+piserver = "http://172.16.15.251:8000/"                #BACKEND | PISERVER URL HERE
 #socket.create_connection(('182.64.172.241', 8000), timeout=2)
 
 @app.route('/')
 #@requires_auth
 def home():
-    #url = piserver + "temp/"
-    #send = requests.get(url, verify = False)
-    return render_template('index.html')
+    url = piserver + "device/"
+
+    r = requests.get(url)
+    # d = json.dumps(str(r))
+    # data = json.load(d)
+    # x = json.loads(str(r), object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+    # return json.loads(r.text)
+    api = json.loads(r.text)
+    home = set()
+    for device in api:
+        home.add(device['room'])
+    return render_template('test.html', devices = api ,home =home)
+    #return json.dumps(data)
+
+
+
 @app.route('/api/')
 def device():
-    url = "https://172.16.15.252:8000/status/"
+    url = piserver + "device/"
     return requests.get(url, verify =False).content
 
 # @app.route('/<string:all_device>/')
@@ -60,4 +59,4 @@ def contact_pi(device,device_id,status):
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5000)))
+    app.run(debug=True, host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5000)))
